d44ecd5a5cd4969a3d2725931cb7a91e60f81dc5
Edwar Sage Jesús Cuya Quispe <Edwar.Cuya@evol.biz>
2024-04-22 17:59:46 -0500

    modificaciones token

--- src/users-service/Ixm.Nexus.Users.Application/Dto/UsersDto/UserDto.cs
+++ src/users-service/Ixm.Nexus.Users.Application/Dto/UsersDto/UserDto.cs
@@ -1,9 +1,12 @@
 ﻿namespace Ixm.Nexus.Users.Application.Dto.UsersDto;
 public class UserDto
 {
-    public string Name { get; set; }
-    public string LastName { get; set; }
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? Phone { get; set; }
+
+    public string Token {  get; set; } = string.Empty;
+    public DateTime ExpirationDate { get; set; }
 }
